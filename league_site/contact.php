9fc626f166563f095b5679c6697132e9173d659b
Conor <chavelin1967@gmail.com>
2024-07-26 13:57:32 +0100

    made an error in my last push and fixed it in the updated code

--- league_site/contact.php
+++ league_site/contact.php
@@ -1,7 +1,8 @@
-<?php require_once 'includes/header.php'; ?>
+<?php require_once 'includes/functions.php'; ?>
 <?php require_once 'includes/header.php'; ?>
 
 <?php
+
 $email = $_REQUEST ['email'];
 $subject = $_REQUEST ['subject'];
 $issue = $_REQUEST ['issue'];
