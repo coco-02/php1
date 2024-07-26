290a6a0b56bb1fc6888b82251f077be20816a438
Conor <chavelin1967@gmail.com>
2024-07-26 10:44:56 +0100

    linked contact.php to mysql so that it can in future store information

--- league_site/contact.php
+++ league_site/contact.php
@@ -1,23 +1,34 @@
 <?php require_once 'includes/header.php'; ?>
+<?php require_once 'includes/header.php'; ?>
 
+<?php
+$email = $_REQUEST ['email'];
+$subject = $_REQUEST ['subject'];
+$issue = $_REQUEST ['issue'];
+$platform = $_REQUEST ['platform'];
+
+$sql = "INSERT INTO contact_us VALUES ('$email','$subject','$issue','$platform')";
+
+if(mysqli_query($conn,$sql)){
+    echo "<h3> Thank you for contacting us we have recieved your query and will 
+    respond within the next 2 buisiness days</h3>";
+}
+
+?>
 <h3><u>Contact Form</u></h3>
 <div class="container">
     <form action="index.php">
-        <label for="name"> Name</label>
-        <input type="text" id="name" name="name" placeholder="Your name.." autocomplete="given-name">
+        <label for="email"> Contact e-mail</label>
+        <input type="text" id="email" name="email" placeholder="Your e-mail" autocomplete="given-name">
         <label for="subject">Subject</label>
-        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-
-        <label for="A"><input type="radio" id="a" name="drone" value="A" checked /> PS5</label>
-        <label for="B"><input type="radio" id="b" name="drone" value="B" checked /> XBOX Series S/X</label>
-        <label for="C"><input type="radio" id="c" name="drone" value="C" checked /> EA Origin</label>
-
-        <label for="avatar">Choose a profile picture:</label>
-
-        <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
-
+        <textarea id="subject" name="subject" ></textarea>
+        <label for ="issue">Write something...</label>
+        <textarea id="issue" name="issue" placeholder="Describe your issue!"></textarea>
+        <label for ="platform">What platform do you use</label>
+        <textarea id="platform" name="platform"></textarea>
         <input type="submit" value="Submit">
     </form>
+    
 </div>
 
 <?php require_once 'includes/footer.php'; ?>
