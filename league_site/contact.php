5d4320de8ecf1946d8c2c5605978343ab7fb0241
Conor <chavelin1967@gmail.com>
2025-03-29 14:10:52 +0000

    Updated contact.php as last code did not work

--- league_site/contact.php
+++ league_site/contact.php
@@ -3,33 +3,27 @@
 
 <?php
 
-$email = $_REQUEST ['email'];
-$subject = $_REQUEST ['subject'];
-$issue = $_REQUEST ['issue'];
-$platform = $_REQUEST ['platform'];
 
-$sql = "INSERT INTO contact_us VALUES ('$email','$subject','$issue','$platform')";
 
-if(mysqli_query($conn,$sql)){
-    echo "<h3> Thank you for contacting us we have recieved your query and will 
-    respond within the next 2 buisiness days</h3>";
-}
 
 ?>
+
 <h3><u>Contact Form</u></h3>
 <div class="container">
-    <form action="index.php">
+    <form method="post" action="contact_submit.php">
         <label for="email"> Contact e-mail</label>
-        <input type="text" id="email" name="email" placeholder="Your e-mail" autocomplete="given-name">
+        <input type="email" id="email" name="email" placeholder="Your e-mail" autocomplete="given-name">
         <label for="subject">Subject</label>
         <textarea id="subject" name="subject" ></textarea>
         <label for ="issue">Write something...</label>
         <textarea id="issue" name="issue" placeholder="Describe your issue!"></textarea>
-        <label for ="platform">What platform do you use</label>
-        <textarea id="platform" name="platform"></textarea>
+        <label for="platform">What platform do you use</label>
+        <select name ="platfrom" id="platform">
+            <option value="ps5">PS5</option>
+            <option value="xbox">Xbox Series S/X</option>
+            <option value="pc">EA Origin</option>
         <input type="submit" value="Submit">
-    </form>
-    
+</form>
 </div>
 
 <?php require_once 'includes/footer.php'; ?>
