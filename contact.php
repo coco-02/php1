e52394dc0ff521f55f13b25930a5770c968df130
Conor <chavelin1967@gmail.com>
2025-05-08 15:00:52 +0100

    contact.php and contact_submit now work properly and data now flows into the database

--- contact.php
+++ contact.php
@@ -1,24 +1,17 @@
 <?php require_once 'includes/functions.php'; ?>
 <?php require_once 'includes/header.php'; ?>
 
-<?php
-
-
-
-
-?>
-
 <h3><u>Contact Form</u></h3>
 <div class="container">
     <form method="post" action="contact_submit.php">
-        <label for="email"> Contact e-mail</label>
-        <input type="email" id="email" name="email" placeholder="Your e-mail" autocomplete="given-name">
+        <label for="contact_email"> Contact e-mail</label>
+        <input type="email" id="contact_email" name="contact_email" placeholder="Your e-mail" autocomplete="given-name">
         <label for="subject">Subject</label>
         <textarea id="subject" name="subject" ></textarea>
         <label for ="issue">Write something...</label>
         <textarea id="issue" name="issue" placeholder="Describe your issue!"></textarea>
-        <label for="platform">What platform do you use</label>
-        <select name ="platfrom" id="platform">
+        <label for="user_platform">What platform do you use</label>
+        <select name ="user_platfrom" id="user_platform">
             <option value="ps5">PS5</option>
             <option value="xbox">Xbox Series S/X</option>
             <option value="pc">EA Origin</option>
--- contact_submit.php
+++ contact_submit.php
@@ -1,22 +1,40 @@
-<?php require_once 'includes/functions.php';
+<?php require_once 'includes/header.php'; ?>
+
+<h3><u>Contact Form</u></h3>
+<div class="container">
+    <form method="post" action="contact_submit.php">
+        <label for="contact_email"> Contact e-mail</label>
+        <input type="email" id="contact_email" name="contact_email" placeholder="Your e-mail" autocomplete="given-name">
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" ></textarea>
+        <label for ="issue">Write something...</label>
+        <textarea id="issue" name="issue" placeholder="Describe your issue!"></textarea>
+        <label for="user_platform">What platform do you use</label>
+        <select id="user_platfrom" name="user_platform">
+            <option value="ps5">PS5</option>
+            <option value="xbox">Xbox Series S/X</option>
+            <option value="pc">EA Origin</option>
+        <input type="submit" value="Submit">
+</form>
+</div>
+<?php require_once 'includes/functions.php'; 
 
 var_dump($_POST);
-
-$email = $_POST ['email'];
-$subject = $_POST ['subject'];
-$issue = $_POST ['issue'];
-$platform = $_POST ['platform'];
-
+if ($_SERVER["REQUEST_METHOD"]=== "POST");
+$email = sanitize_input($_POST['contact_email']);
+$subject = sanitize_input ($_POST['subject']);
+$issue = sanitize_input ($_POST['issue']);
+$platform = sanitize_input ($_POST['user_platform']);
 // $sql = "INSERT INTO contact_us VALUES ('$email','$subject','$issue','$platform')";
 
 $conn = f1_get_db_connection();
-$sql = "INSERT INTO contact_us ('contact_email','subject','issue','user_platform') VALUES (?,?,?,?)";
-$stmt= $conn->prepare($sql);
-$stmt->execute([$email, $subject, $issue, $platorm]);
-if(mysqli_query($pdo,$sql)){
-    echo "<h3> Thank you for contacting us we have recieved your query and will 
-    respond within the next 2 buisiness days</h3>";
-}
+$sql = "INSERT INTO contact_us (contact_email, subject, issue, user_platform) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$result = $stmt->execute([$email, $subject, $issue, $platform]);
+// if(mysqli_query($pdo,$sql)){
+//     echo "Thank you for contacting us we have recieved your query and will 
+//     respond within the next 2 buisiness days";
+// }
 
 const EMAIL_REQUIRED = 'Please enter your email';
 const EMAIL_INVALID = 'Please enter a valid email';
@@ -31,3 +49,12 @@ if ($email) {
 } else {
     $errors['email'] = EMAIL_REQUIRED;
 }
+
+if($result){
+    echo "Thank you for contacting us we have recieved your query and will 
+    respond within the next 2 buisiness days";
+}  else {
+    echo "Error: " . $stmt->errorInfo()[2]; // Display error message
+}
+
+require_once 'includes/footer.php';?>
